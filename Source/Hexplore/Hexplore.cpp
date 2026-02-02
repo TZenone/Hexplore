ef89cb9f8c71cc6bdceb47eed8cfd771b71fd0f1
TZenone <tristan.zenone@gmail.com>
2026-02-02 10:02:22 +0100

    Inital project files

---
+++ Source/Hexplore/Hexplore.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Hexplore.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Hexplore, "Hexplore" );
