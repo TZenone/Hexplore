41b9f821e478496fb303406ea8a60f80ba8aecf6
TZenone <tristan.zenone@gmail.com>
2026-02-02 12:23:10 +0100

    Input init

--- Source/Hexplore/Private/Player/HexplorePlayerController.cpp
+++ Source/Hexplore/Private/Player/HexplorePlayerController.cpp
@@ -2,4 +2,28 @@
 
 
 #include "Player/HexplorePlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+AHexplorePlayerController::AHexplorePlayerController()
+{
+	bReplicates = true;
+}
+
+void AHexplorePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(HexploreContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+
+	Subsystem->AddMappingContext(HexploreContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
