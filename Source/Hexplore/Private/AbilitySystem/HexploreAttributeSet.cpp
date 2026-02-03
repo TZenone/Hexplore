51cdc8663b64602a91324f8cb87d17cc2c70a42f
TZenone <tristan.zenone@gmail.com>
2026-02-03 13:41:57 +0100

    Health and Stamina attributes in AS, along with their getters

--- Source/Hexplore/Private/AbilitySystem/HexploreAttributeSet.cpp
+++ Source/Hexplore/Private/AbilitySystem/HexploreAttributeSet.cpp
@@ -2,4 +2,42 @@
 
 
 #include "AbilitySystem/HexploreAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+UHexploreAttributeSet::UHexploreAttributeSet()
+{
+	InitMaxHealth(100.f);
+	InitHealth(100.f);
+	InitMaxStamina(100.f);
+	InitStamina(100.f);
+}
+
+void UHexploreAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UHexploreAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UHexploreAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UHexploreAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UHexploreAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
+}
+
+void UHexploreAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHexploreAttributeSet, Health, OldHealth);
+}
+
+void UHexploreAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHexploreAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UHexploreAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHexploreAttributeSet, Stamina, OldStamina);
+}
+
+void UHexploreAttributeSet::OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHexploreAttributeSet, MaxStamina, OldMaxStamina);
+}
--- Source/Hexplore/Public/AbilitySystem/HexploreAttributeSet.h
+++ Source/Hexplore/Public/AbilitySystem/HexploreAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "HexploreAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +20,39 @@ UCLASS()
 class HEXPLORE_API UHexploreAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UHexploreAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, Replicated, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UHexploreAttributeSet, Health);
+
+	UPROPERTY(BlueprintReadOnly, Replicated, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UHexploreAttributeSet, MaxHealth);
+
+	UPROPERTY(BlueprintReadOnly, Replicated, ReplicatedUsing = OnRep_Stamina, Category = "Vital Attributes")
+	FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS(UHexploreAttributeSet, Stamina);
+
+	UPROPERTY(BlueprintReadOnly, Replicated, ReplicatedUsing = OnRep_MaxStamina, Category = "Vital Attributes")
+	FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS(UHexploreAttributeSet, MaxStamina);
 	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Stamina(const FGameplayAttributeData& OldStamina) const;
+
+	UFUNCTION()
+	void OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const;
 };
+
+
