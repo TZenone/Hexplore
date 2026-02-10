1c61b752137b35f64b4f907bb453a1a62154df63
TZenone <tristan.zenone@gmail.com>
2026-02-10 15:11:15 +0100

    AutoAttackWorking

--- Source/Hexplore/Private/AbilitySystem/Abilities/HexploreAttack.cpp
+++ Source/Hexplore/Private/AbilitySystem/Abilities/HexploreAttack.cpp
@@ -5,40 +5,10 @@
 
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
-#include "Actor/HexploreMelee.h"
 #include "Interaction/CombatInterface.h"
 
 void UHexploreAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 	
-}
-
-void UHexploreAttack::SpawnAttack(const FVector& TargetLocation)
-{
-	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
-	if (!bIsServer) return;
-
-	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
-	if (CombatInterface)
-	{
-		const FVector SocketLocation = CombatInterface->GetBuffSocketLocation();
-
-		FTransform SpawnTransform;
-		SpawnTransform.SetLocation(SocketLocation);
-	
-		AHexploreMelee* Melee = GetWorld()->SpawnActorDeferred<AHexploreMelee>(
-			MeleeClass,
-			SpawnTransform,
-			GetOwningActorFromActorInfo(),
-			Cast<APawn>(GetOwningActorFromActorInfo()),
-			ESpawnActorCollisionHandlingMethod::AlwaysSpawn
-		);
-
-		const UAbilitySystemComponent* SourceASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo());
-		const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
-		Melee->DamageEffectSpecHandle = SpecHandle;
-		
-		Melee->FinishSpawning(SpawnTransform);
-	}
 }
--- Source/Hexplore/Public/AbilitySystem/Abilities/HexploreAttack.h
+++ Source/Hexplore/Public/AbilitySystem/Abilities/HexploreAttack.h
@@ -19,10 +19,6 @@ class HEXPLORE_API UHexploreAttack : public UHexploreGameplayAbility
 protected:
 	
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
-	void SpawnAttack(const FVector& TargetLocation);
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TSubclassOf<AHexploreMelee> MeleeClass;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TSubclassOf<UGameplayEffect> DamageEffectClass;
